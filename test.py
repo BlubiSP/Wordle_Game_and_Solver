d7ccb4e2876fa3831dd9a41b36295b798cf7ea9e
BlubiSP <stefan.poeck@live.de>
2022-02-16 16:49:53 +0100

    testing git

--- test.py
+++ test.py
@@ -2,9 +2,15 @@ import random
 import requests
 
 r = requests.get("https://rhdzmota.com/files/wordle.json", json=True)
-possible_words = (r.json())
+words_list = (r.json())
 
-def get_answer(possible_words = possible_words):
-    return random.choice(possible_words)
-# na
-print(get_answer())
+def get_answer(words_list = words_list):
+    return random.choice(words_list)
+
+def valid_guess(guess, words_list = words_list):
+    if guess.lower() in words_list:
+        return True
+    return False
+
+
+print(valid_guess("clane"))
