08f759fe73c6589283ba447915d2de9e817e82fd
BlubiSP <stefan.poeck@live.de>
2022-02-16 16:39:42 +0100

    initial

---
+++ test.py
@@ -0,0 +1,8 @@
+import random 
+import requests
+r = requests.get("https://rhdzmota.com/files/wordle.json", json=True)
+possible_words = (r.json())
+def get_answer(possible_words = possible_words):
+    return random.choice(possible_words)
+
+print(get_answer())
