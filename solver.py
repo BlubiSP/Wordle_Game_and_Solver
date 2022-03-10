8c7f00b0f735b1e7c0b431054fce6105b40b7aaf
BlubiSP <stefan.poeck@live.de>
2022-03-10 15:25:56 +0100

    Add some error handling + little cleaning

--- solver.py
+++ solver.py
@@ -10,45 +10,65 @@ def main():
         correctPosition = []
         notinWord = []
         returndic = {}
-        print("Press enter when you are done")
-        _ = input()
+
+        _ = input("Press enter when you are done\n")
         print("Any correct Letters AND correct position? (GREEN)")
         print("Enter them in this format:")
         print("(letter position, letter position) Example: a1, p2, p3, l4, e5")
-        print("If there are none just press enter.")
-        correctletterandposition = input()
+        correctletterandposition = input("If there are none just press enter.\n")
         if len(correctletterandposition) > 0:
-            correctPosition = functions.conv_into_class(correctletterandposition)
+            try:
+                correctPosition = functions.conv_into_class(correctletterandposition)
+           
+            except ValueError:
+                print("ERROR: Invalid input\nIgnoring input.")
+
         print("Any correct Letters? (ORANGE)")
         print("Enter them in this format:")
         print("(letter position, letter position) Example: a1, p2, p3, l4, e5")
-        print("If there are none just press enter.")
-        correctletter = input()
+        correctletter = input("If there are none just press enter.\n")
         if len(correctletter) > 0:
-            inWord = functions.conv_into_class(correctletter)
+            try:
+                inWord = functions.conv_into_class(correctletter)
+
+            except ValueError:
+                print("ERROR: Invalid input\nIgnoring input.")
+
         print("Any incorrect Letters? (GREY)")
         print("Enter them in this format:")
         print("(letter, letter) Example: a, p, p, l, e")
-        print("If there are none just press enter.")
-        incorrectLetters = input()
+        incorrectLetters = input("If there are none just press enter.\n")
         if len(incorrectLetters) > 0:
-            notinWord = incorrectLetters.replace(" ", "").split(",")
+            try:
+                notinWord = incorrectLetters.replace(" ", "").split(",")
+
+            except ValueError:
+                print("ERROR: Invalid input\nIgnoring input.")
+
         returndic["inWord"] = inWord
         returndic["correctPosition"] = correctPosition
         returndic["notinWord"] = notinWord
         list_of_possible_answers = cheats.get_list_of_possible_answers(returndic, possible_answers)
         return list_of_possible_answers
+
     print("Finished Loading")
+
     while len(guess) != 1:
 
         if isinstance(guess, str):
             print(f"Guess {guess}!")
+
         else:
-            print(guess)
-            print(f"Guess {random.choice(guess)}!")
+            if len(guess) == 0:
+                print("ERROR: 0 Possible answers.\nYou made a wrong input. Try again!")
+                quit()
+            else:
+                print(f"Guess {random.choice(guess)}!")
+
         guess = get_guess()
-    print(f"The answer is {guess[0]}!")
     
+    print(f"The answer is {guess[0]}!")
+
 
 if __name__ == "__main__":
     main()
