b180d677c52a02ec95ca8b79f1314e6581665efd
BlubiSP <stefan.poeck@live.de>
2022-03-31 07:02:03 +0200

    bugfix

--- simulation.py
+++ simulation.py
@@ -1,16 +1,16 @@
 def main():
     import functions, random, cheats, time
     num_of_simulation = input("Enter number of simulations:\n")
-    guess = input("Enter guess ( has to be 5 letters")
+    guess = input("Enter guess ( has to be 5 letters)")
     if len(guess) != 5 or not guess.isalpha():
-        raise ValueError("Invalid guess. Exiting program")
+        raise ValueError("Invalid guess\n")
         time.sleep(3)
         exit()
     possible_answers = functions.read_list("answers.txt")
     #starting guess
     guess = "crane"
     answer = random.choice(possible_answers)
-
+    
     def guessing(list_of_possible_answers, guess, answer):
         new_list_of_answers = cheats.get_list_of_possible_answers(functions.evaluate(guess, answer), list_of_possible_answers)
         guess = random.choice(new_list_of_answers)
@@ -30,7 +30,6 @@ def main():
         # data = [number of tries, answer]
         data = game(guess, possible_answers, answer)
         # If the programm needs more then 6 tries it will print the last guess and the answer here
-        # I simulated 10k games and it could not loose
         if data[0] > 6:
             lost += 1
             print(f"Number of Guesses = {data[0]}\nAnswer = {data[1]}")
